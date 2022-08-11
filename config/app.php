eeb4ae18de4d7e4dd5a9e874ad7ba855adca0ccd
ObjectRetros <dennisobject@gmail.com>
2022-08-11 04:08:52 +0200

    :sparkles: Add permission system

--- config/app.php
+++ config/app.php
@@ -195,6 +195,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        App\Providers\FortifyServiceProvider::class,
+
     ],
 
     /*
