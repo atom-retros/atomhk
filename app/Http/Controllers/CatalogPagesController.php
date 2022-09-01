316146a450fafaabc2de016f512f0a2f7554e4ed
ObjectRetros <dennisobject@gmail.com>
2022-09-01 21:46:50 +0200

    Purify page text details

--- app/Http/Controllers/CatalogPagesController.php
+++ app/Http/Controllers/CatalogPagesController.php
@@ -35,7 +35,7 @@ class CatalogPagesController extends Controller
             'page_teaser' => $request->input('page_teaser'),
             'page_text1' => $request->input('page_text1'),
             'page_text2' => $request->input('page_text2'),
-            'page_text_details' => $request->input('page_text_details'),
+            'page_text_details' => clean($request->input('page_text_details')),
         ]);
 
         return to_route('catalog-pages.index')->with('success', 'The catalog page has been created!');
